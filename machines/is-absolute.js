6df9626276bd6edeed274d57f2517c9d6f6d6cb8
Scott Gress <scottmgress@gmail.com>
2016-07-29 15:41:20 -0500

    Refactor "if-absolute" into "is-absolute" to stay in line with other boolean-ish machines.

--- machines/is-absolute.js
+++ machines/is-absolute.js
@@ -1,16 +1,16 @@
 module.exports = {
 
 
-  friendlyName: 'If path is absolute',
+  friendlyName: 'Is path absolute?',
 
 
-  description: 'Determine whether or not a is absolute.',
+  description: 'Determine whether or not a path is absolute.',
 
 
   extendedDescription: 'An absolute (or "full") path points to the same location in a file system regardless of the current working directory (e.g. "/usr/local/foo.zip"). To do that, it must contain the root directory (`/`).  By contrast, a relative path starts from some given working directory, avoiding the need to provide the full absolute path (e.g. "foo/thumb.png" or "./bon-jovi.mov" or "../../hardcore.midi".)  The same principle applies when considering URL paths-- except that, instead of the root directory, they\'re absolute in the context of the web root ("/") or a particular domain (e.g. "google.com").  This machine works for filesystem paths (Mac, *nix, and Windows) as well as web-root-relative URL paths. It does not work for URLs that include other parts, such as the protocol (http://), domain (google.com), port (:1337), querystring (?foo=bar), or fragment (#itsme-again-hank-the-cowdog).',
 
 
-  sideEffects: 'idempotent',
+  sideEffects: 'cacheable',
 
 
   sync: true,
@@ -30,11 +30,9 @@ module.exports = {
   exits: {
 
     success: {
-      description: 'The specified string was determined to be an absolute path.'
-    },
-
-    notAbsolute: {
-      description: 'The specified string was not determined to be an absolute path.'
+      outputFriendlyName: 'Is absolute path?',
+      outputDescription: 'Whether the specified path is absolute or not.',
+      outputExample: true
     },
 
   },
@@ -45,10 +43,14 @@ module.exports = {
     // Use `path-is-absolute` polyfill for Node <= 0.12
     var pathIsAbsolute = require('path-is-absolute');
 
+    // If the path is absolute, return `true` through the `success` exit.
     if (pathIsAbsolute(inputs.path)) {
-      return exits.success();
+      return exits.success(true);
     }
-    return exits.notAbsolute();
+
+    // Otherwise return `false` through the `success` exit.
+    return exits.success(false);
+
   }
 
 
